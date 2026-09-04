4bb6d45f68f9a13f20aeca63d144bc7a16ce8c0d
Ranee <deguzman.rv@obmontessori.edu.ph>
2026-09-04 11:57:30 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+#De Guzman, Cruz
+studname = "Sophia Cruz" #string
+studage = 14 #integer
+studheight1 = 150.8 #float
+countries_visit = ["Cananda", "Russia", "Japan"] #list
+student_type = False #boolean
+stud_pref = {
+    "color": "Teal",
+    "car brand": "Toyota",
+    "shoe size": 6.5,
+    "bff": "Everyone"
+    }
+
+stud_fruits = {
+    "Strawberry", 
+    "Cherry", 
+    "Chili Pepper", 
+    "Apple", 
+    "Jackfruit"
+    } #set
+
+days_week = (
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday", 
+    "Sunday"
+)
+
+print("Name:", studname)
+print("Age:", studage)
+print("Height:", studheight1)
+print("Countries Visited:", countries_visit)
+print("Student Type:", student_type)
+print("Preferences:", stud_pref)
+print("Favorite Fruits:", stud_fruits)
+print("Days of the Week:", days_week)
